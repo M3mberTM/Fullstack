f3af33d08952bbbdada74d5ac5c11137cdc41e0a
M3mberTM <137719297+M3mberTM@users.noreply.github.com>
2024-05-30 12:23:03 +0200

    Part 2.9 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,21 @@ import {useState} from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        {name: 'Arto Hellas', phone:'00-494'}
+        { name: 'Arto Hellas', number: '040-123456', id: 1 },
+        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
     ])
     const [newName, setNewName] = useState('')
     const [newPhone, setNewPhone] = useState('')
+    const [newFilter, setNewFilter] = useState('')
+
+    const handleFilterChange = (event) => {
+        const newFilterVal = event.target.value
+        setNewFilter(newFilterVal)
+
+
+    }
     const handleNameChange = (event) => {
         setNewName(event.target.value)
     }
@@ -18,7 +29,8 @@ const App = () => {
         event.preventDefault()
         const newPerson = {
             name: newName,
-            phone: newPhone
+            number: newPhone,
+            id: persons.length
         }
         let objExists = false
         for (let i = 0; i < persons.length; i++) {
@@ -41,6 +53,8 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>filter show with <input value={newFilter} onChange={handleFilterChange}/></div>
+            <h2>add a new</h2>
             <form>
                 <div>
                     name: <input value={newName} onChange={handleNameChange}/>
@@ -52,7 +66,10 @@ const App = () => {
             </form>
             <h2>Numbers</h2>
             {persons.map((person) => {
-                return <p key={person.name}>{person.name} {person.phone}</p>
+                if (person.name.toLowerCase().includes(newFilter.toLowerCase())) {
+                    return <p key={person.id}>{person.name} {person.number}</p>
+                }
+
             })}
         </div>
     )
