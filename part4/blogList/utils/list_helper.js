c49fc59d04c422d3513d23283e2eb592a6a1433b
M3mberTM <137719297+M3mberTM@users.noreply.github.com>
2024-06-13 16:06:39 +0200

    Part 4.4 is done

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    let likes = 0
+    for (let i = 0; i < blogs.length; i++) {
+        likes = likes + blogs[i].likes
+    }
+
+    return likes
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
