b1e7f657c4928849f0b2c65463f33e2a33a21b21
M3mberTM <137719297+M3mberTM@users.noreply.github.com>
2024-06-18 18:17:12 +0200

    4.17 is done

--- part4/blogList/controllers/users.js
+++ part4/blogList/controllers/users.js
@@ -3,7 +3,7 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.post('/', async (request, response) => {
-    const { username, name, password } = request.body
+    const {username, name, password} = request.body
 
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(password, saltRounds)
@@ -21,8 +21,8 @@ usersRouter.post('/', async (request, response) => {
 
 
 usersRouter.get('/', async (request, response) => {
-    const blogs = await User.find({})
-    response.json(blogs)
+    const users = await User.find({}).populate('blogs', {title: 1, author: 1, url: 1, id: 1})
+    response.json(users)
 })
 
 module.exports = usersRouter
