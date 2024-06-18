226ffb25444609c88de27f4a9648867b19d0c46a
M3mberTM <137719297+M3mberTM@users.noreply.github.com>
2024-06-18 18:35:16 +0200

    4.19 is done

--- part4/blogList/controllers/blogs.js
+++ part4/blogList/controllers/blogs.js
@@ -1,8 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
-const mongoose = require("mongoose");
 
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
 blogsRouter.get('/', async (request, response) => {
     const result = await Blog.find({}).populate('user', {username: 1, name: 1, id: 1})
     response.json(result)
@@ -12,7 +20,12 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
 
-    const user = await User.findById(body.userId)
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+
 
 
     const blog = new Blog({
