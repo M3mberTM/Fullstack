8acb2000f6733d2944a287fb43e8b1fa42f40617
M3mberTM <137719297+M3mberTM@users.noreply.github.com>
2024-06-22 19:00:40 +0200

    5.5 is done

--- part5/bloglist-frontend/src/components/BlogList.jsx
+++ part5/bloglist-frontend/src/components/BlogList.jsx
@@ -1,5 +1,6 @@
 import Blog from "./Blog.jsx";
 import BlogForm from "./BlogForm.jsx";
+import Toggleable from "./Toggleable.jsx";
 
 const BlogList = ({blogs, user, handleLogout, author, title, url, setTitle, handleNewBlog, setAuthor, setUrl}) => {
 
@@ -7,8 +8,10 @@ const BlogList = ({blogs, user, handleLogout, author, title, url, setTitle, hand
         <h1>blogs</h1>
         <p>{user.username} is logged in <button onClick={handleLogout}>Log out</button></p>
         <h3>Create a blog</h3>
-        <BlogForm author={author} title={title} setTitle={setTitle} handleNewBlog={handleNewBlog} setAuthor={setAuthor} setUrl={setUrl} url={url}/>
-
+        <Toggleable buttonLabel={"new note"}>
+            <BlogForm author={author} title={title} setTitle={setTitle} handleNewBlog={handleNewBlog}
+                      setAuthor={setAuthor} setUrl={setUrl} url={url}/>
+        </Toggleable>
         <h3>Created Blogs</h3>
         {blogs.map(blog =>
             <Blog key={blog.id} blog={blog}/>
