d553d20775d2c63fd167ccac7504f767f544d54b
M3mberTM <137719297+M3mberTM@users.noreply.github.com>
2024-06-24 13:09:37 +0200

    5.14 is done

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,39 +1,78 @@
 import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from "./Blog.jsx";
 
-test('renders content', () => {
 
-    const blog = {
-        title: "Content",
-        author: "Richard",
-        url: "youtube.com",
-        likes: 40,
-        user: {
-            username: "m3mber"
+describe("Rendering without events", () => {
+    test('renders content', () => {
+
+        const blog = {
+            title: "Content",
+            author: "Richard",
+            url: "youtube.com",
+            likes: 40,
+            user: {
+                username: "m3mber"
+            }
         }
-    }
-    render(<Blog blog={blog}/>)
+        render(<Blog blog={blog}/>)
 
-    const title = screen.getByText(blog.title, {exact: false})
-    const author = screen.getByText(blog.author, {exact: false})
-    expect(title).toBeDefined()
-    expect(author).toBeDefined()
+        const title = screen.getByText(blog.title, {exact: false})
+        const author = screen.getByText(blog.author, {exact: false})
+        expect(title).toBeDefined()
+        expect(author).toBeDefined()
+    })
+
+    test("doesn't render toggleable content", () => {
+
+        const blog = {
+            title: "Content",
+            author: "Richard",
+            url: "youtube.com",
+            likes: 40,
+            user: {
+                username: "m3mber"
+            }
+        }
+        const {container} = render(<Blog blog={blog}/>)
+
+        const element = container.querySelector(".note")
+
+        expect(element).not.toHaveTextContent(blog.url)
+    })
+
 })
 
-test("doesn't render toggleable content", () => {
 
-    const blog = {
-        title: "Content",
-        author: "Richard",
-        url: "youtube.com",
-        likes: 40,
-        user: {
-            username: "m3mber"
+describe("Rendering with user events", () => {
+
+    test("Url and likes are rendered after toggling the element", async () => {
+        const blog = {
+            title: "Content",
+            author: "Richard",
+            url: "youtube.com",
+            likes: 40,
+            user: {
+                username: "m3mber"
+            }
         }
-    }
-    const { container} = render(<Blog blog={blog}/>)
+        const {container} = render(<Blog blog={blog}/>)
 
-    const element = container.querySelector(".note")
-    screen.debug()
-    expect(element).not.toHaveTextContent(blog.url)
+        const user = userEvent.setup()
+        const button = container.querySelector(".blogToggleButton")
+        await user.click(button)
+
+
+        const element = container.querySelector(".note")
+
+        expect(element).toHaveTextContent(blog.url)
+        expect(element).toHaveTextContent(blog.likes)
+    })
+})
+
+describe('Event handlers', () => {
+
+    test('Like handling', () => {
+
+    })
 })
