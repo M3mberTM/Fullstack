283c82e40f203b880c7b96ca656c7cf32fc95e82
M3mberTM <137719297+M3mberTM@users.noreply.github.com>
2024-07-19 13:55:00 +0200

    6.14 is done

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -2,8 +2,19 @@ import AnecdoteForm from "./components/AnecdoteForm.jsx";
 import AnecdoteList from "./components/AnecdoteList.jsx";
 import Filter from "./components/Filter.jsx";
 import Notification from "./components/Notification.jsx";
+import {useDispatch} from "react-redux";
+import {useEffect} from "react";
+import anecdoteService from "./services/anecdotes.js";
+import {setNotes} from "./reducers/anecdoteReducer.js";
 
 const App = () => {
+
+    const dispatch = useDispatch()
+    useEffect(() => {
+        anecdoteService.getAll().then(anecdotes => {
+            dispatch(setNotes(anecdotes))
+        })
+    }, [])
 
     return (
         <div>
