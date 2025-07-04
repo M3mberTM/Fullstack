56e91b00c7d652bb02c14bfa987ce40d51ba7af7
M3mberTM <137719297+M3mberTM@users.noreply.github.com>
2025-07-04 19:16:08 +0200

    7.17 done

--- part7/blogapp/bloglist-frontend/src/components/Header.jsx
+++ part7/blogapp/bloglist-frontend/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import Login from "./Login.jsx";
+import {Link} from "react-router-dom";
 
 const Header = ({user, handleLogout, handleLogin}) => {
 
@@ -14,12 +15,17 @@ const Header = ({user, handleLogout, handleLogin}) => {
    }
 
     return (
-        <div id={"header"}>
-            <h1>blogs</h1>
-            <p>
-                {user.username} is currently logged in{' '}
-                <button onClick={handleLogout}>Log out</button>
-            </p>
+        <div id={"header"} style={{display: "flex", backgroundColor: "#94bdff"}}>
+            <div>
+                <Link to={"/"} style={{marginRight: "5px"}}>Blogs</Link>
+                <Link to={"/users"} style={{marginRight: "5px"}}>Users</Link>
+            </div>
+            <div>
+                <p style={{margin: "0"}}>
+                    {user.username} is currently logged in{' '}
+                    <button onClick={handleLogout}>Log out</button>
+                </p>
+            </div>
         </div>
     )
 
